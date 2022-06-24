45f9201fe2aa747f74a1eea32855bc52337c1eae
Avara, Emre <emre_avara@hotmail.com>
2022-06-25 01:29:15 +0200

    Add post and get methods for author

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,27 @@
 var express = require('express')
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+const Author = require('./models/author')
 
 var app = express()
+app.use(bodyParser.json())
+
+app.post('/author', (request, response) =>{
+    var authorData = request.body
+    var author = new Author(authorData)
+    author.save((error, result)=>{
+        if(error){
+            console.log(error)
+            return response.sendStatus(500).send({message:error})
+        }
+        return response.sendStatus(201)
+    })
+})
+
+app.get("/author", async (request, response)=>{
+    var authors = await Author.find({},"-__v");
+    response.send(authors)
+})
 
 mongoose.connect('mongodb+srv://emrea:24121994@cluster0.omknufb.mongodb.net/?retryWrites=true&w=majority', (error)=>{
     if(!error){
